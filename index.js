2f8593d0e903ad34ec345670202c99bae538c644
dhirajko <dhirajkoirala2013@gmail.com>
2019-02-13 20:22:19 +0200

    adding emmit and listening  in client and server

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const server=app.listen(3000, ()=>{
     
 })
 
-const io=socket(server);
+const io=socket(server);                                        // connecting the socket in the express server on port 3000
 io.on('connection',function(socket) {
-    console.log('made socket connection');
+    console.log('made socket connection',socket.id);
     
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat',data)
+    })
+
 })
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,19 @@
-const socket= io.connect('http://localhost:3000')
+const socket= io.connect('http://localhost:3000')                   // connection to the localhost
+
+// Dom 
+const output= document.getElementById('output');
+const handle= document.getElementById('handle');
+const message= document.getElementById('message');
+const send= document.getElementById('send');
+
+
+send.addEventListener('click',()=>{                             // emiting message to the server
+    socket.emit('chat',{
+        message: message.value,
+        handle: handle.value
+    });
+})
+
+socket.on('chat', (data)=>{
+    output.innerHTML+='<p><strong>'+data.handle+'</strong>' + data.message+ '</p>'
+})
